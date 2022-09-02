1141caf37a4da0d5589222422bacec0e212aa0f5
abyanjamil <112526178+abyanjamil@users.noreply.github.com>
2022-09-02 14:23:06 +0700

    Add files via upload

---
+++ python4-tkj4.py
@@ -0,0 +1,48 @@
+# Rumus Persegi sisi * sisi
+
+sisi1 = 20
+sisi2 = 20
+luas_p = sisi1 * sisi2
+print("Luas persegi adlah", luas_p)
+
+# Rumus persegi panjang Panjang * Lebar
+
+panjang = 10
+lebar = 20
+luas_pp = panjang * lebar
+
+print("luas persegi panjang adalah", luas_pp)
+
+# rumus Trapesium 1/2 * (a+b) * tinggi
+print("\nTrapesium")
+
+a = 6
+b = 10
+t = 8
+
+luas_t = (a+b) * t / 2
+print("Luas Trapesium adalah", luas_t)
+
+# rumus segitiga 1/2 * a * t
+print("\nSegitiga")
+a = 30
+t = 30
+luas_s = a * t / 2
+print("Luas Segitiga adalah", luas_s)
+
+# rumus Jajar Genkjang a * t
+
+a = 40
+t = 10
+luas_jg = a * t
+print("Luas Jajar Genjang", luas_jg)
+
+# rumus Lingkaran n * r * r * L
+
+phi = 3.14
+r = 6
+r = 10
+l = 20
+luas_l = phi * r * r * l
+print("luas Lingkaran", luas_l)
+
