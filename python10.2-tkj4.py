aa643d60de1f8667495e987495ddbf39be432f61
abyanjamil <112526178+abyanjamil@users.noreply.github.com>
2022-10-12 14:07:23 +0700

    Add files via upload

---
+++ python10.2-tkj4.py
@@ -0,0 +1,46 @@
+#membuat tipe data koleksi
+
+koleksi_data_str = ["pisang","mangga","jambu","semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["Rizky Billar", 100, True, "Reza Arap"]
+
+print("koleksi data string:", koleksi_data_str)
+
+print("koleksi data integer:", koleksi_data_int)
+
+print("koleksi data campuran:", koleksi_data_mix)
+
+
+koleksi_nama_hewan = ["Ubur ubur","Macan","Gurita","Cacing"]
+
+koleksi_data_int = [100, 100, 100, 100]
+
+koleksi_nama_teman = ["Rendra","Jamil","Rasyid Ridho","Opai"]
+
+print("koleksi data string:", koleksi_nama_hewan)
+
+print("koleksi data integer:", koleksi_data_int)
+
+print("koleksi data string:", koleksi_nama_teman)
+
+#tampilkan data koleksi dengan indeks
+
+#data ke 2 nama hewan
+
+print("Nama Hewan Data ke-2:", koleksi_nama_hewan[1])
+
+#data pertama nilai UTS 
+
+print("Nilai PTS Data ke-1:", koleksi_data_int[0])
+
+
+
+
+
+
+
+
+
+
